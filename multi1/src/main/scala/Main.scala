9dd427948e530a5e7bddc182266ea1af39f73068
Rustam Ismailov <rismailov@competo.io>
2018-12-27 11:41:30 +0400

    Small improvements

--- multi1/src/main/scala/Main.scala
+++ multi1/src/main/scala/Main.scala
@@ -2,10 +2,10 @@ import monocle.macros.GenLens
 
 object Main extends App {
   println("multi1 can use common sub-project")
-
   val entity = Entity("id", NestedEntity("value"))
+  println(s"entity = $entity")
 
   println("multi1 can use monocle dependency")
-
   val idLens = GenLens[Entity](_.id)
+  println(s"idLens = $idLens")
 }
